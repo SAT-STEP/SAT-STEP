fe7da759c0fc8b2f69f04692569291efb0a33ba9
Simo Soini <simo@capscience.fi>
2023-09-14 18:43:50 +0300

    gui: Added a scrollarea with to show constraints.

--- src/gui/constraint_list.rs
+++ src/gui/constraint_list.rs
@@ -1,5 +1,14 @@
-use egui::{Response, Ui};
+use egui::{Response, Ui, ScrollArea};
 
 pub fn constraint_list(ui: &mut Ui) -> Response {
-    ui.label("Constraint list")
+    let constraints: Vec<&[i32]> = vec![&[123, 43, 829, 432], &[-123, 32, 543], &[53]];
+    ui.vertical(|ui| {
+        ScrollArea::vertical()
+            .stick_to_bottom(true)
+            .show(ui, |ui| {
+                for constraint in constraints {
+                    ui.label(format!("{:?}", constraint));
+                }
+        });
+    }).response
 }
