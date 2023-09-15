3cf99578f0bcd0078766202a5fec7b24f17aa596
Simo Soini <simo@capscience.fi>
2023-09-15 15:04:32 +0300

    Added test for solve_sudoku function.

--- src/service.rs
+++ src/service.rs
@@ -60,4 +60,28 @@ mod tests {
         ];
         assert_eq!(sudoku, should_be);
     }
+
+    #[test]
+    fn test_solve_sudoku() {
+        use super::*;
+
+        let sudoku = get_sudoku("data/sample_sudoku.txt".to_string());
+        let mut solver = cadical::Solver::with_config("plain").unwrap();
+        let callback_wrapper = CadicalCallbackWrapper::new();
+        solver.set_callbacks(Some(callback_wrapper.clone()));
+
+        let solved = solve_sudoku(&sudoku, &mut solver).unwrap();
+        let should_be = vec![
+            vec![Some(6), Some(9), Some(3), Some(7), Some(8), Some(4), Some(5), Some(1), Some(2)],
+            vec![Some(4), Some(8), Some(7), Some(5), Some(1), Some(2), Some(9), Some(3), Some(6)],
+            vec![Some(1), Some(2), Some(5), Some(9), Some(6), Some(3), Some(8), Some(7), Some(4)],
+            vec![Some(9), Some(3), Some(2), Some(6), Some(5), Some(1), Some(4), Some(8), Some(7)],
+            vec![Some(5), Some(6), Some(8), Some(2), Some(4), Some(7), Some(3), Some(9), Some(1)],
+            vec![Some(7), Some(4), Some(1), Some(3), Some(9), Some(8), Some(6), Some(2), Some(5)],
+            vec![Some(3), Some(1), Some(9), Some(4), Some(7), Some(5), Some(2), Some(6), Some(8)],
+            vec![Some(8), Some(5), Some(6), Some(1), Some(2), Some(9), Some(7), Some(4), Some(3)],
+            vec![Some(2), Some(7), Some(4), Some(8), Some(3), Some(6), Some(1), Some(5), Some(9)],
+        ];
+        assert_eq!(solved, should_be);
+    }
 }
