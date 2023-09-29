21dab155e091c7c3fd531e2b58dda1e5bd38f320
Simo Soini <simo@capscience.fi>
2023-09-29 20:22:51 +0300

    Fixed merge conflicts

--- src/lib.rs
+++ src/lib.rs
@@ -106,6 +106,18 @@ impl ListFilter {
         self.apply_filters()
     }
 
+    pub fn clear_cell(&mut self) -> Vec<Vec<i32>> {
+        self.cell_filter = (0..self.constraints.borrow().len()).collect();
+        self.apply_filters()
+    }
+
+    pub fn clear_all(&mut self) -> Vec<Vec<i32>> {
+        // This will apply the filters several times, but avoids code repetition
+        // Change if this becomes a problem!
+        self.clear_length();
+        self.clear_cell()
+    }
+
     pub fn by_cell(&mut self, row: i32, col: i32) -> Vec<Vec<i32>> {
         if let Some(cell_set) = self.cell_constraints.get(&(row, col)) {
             self.cell_filter = cell_set.clone()
@@ -118,17 +130,6 @@ impl ListFilter {
         self.apply_filters()
     }
 
-    pub fn clear_cell(&mut self) -> Vec<Vec<i32>> {
-        self.cell_filter = (0..self.constraints.borrow().len()).collect();
-        self.apply_filters()
-    }
-
-    pub fn clear_all(&mut self) -> Vec<Vec<i32>> {
-        // This will apply the filters several times, but avoids code repetition
-        // Change if this becomes a problem!
-        self.clear_length();
-        self.clear_cell()
-    }
 }
 
 pub fn solve_sudoku(
