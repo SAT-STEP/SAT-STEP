e5c84d56e806313099be957242f3288f97b75421
helmer <helmer.lauronen@gmail.com>
2023-10-04 10:53:02 +0300

    Grey out clue cells

--- src/gui/constraint_list.rs
+++ src/gui/constraint_list.rs
@@ -28,6 +28,7 @@ impl SATApp {
                     match sudoku_result {
                         Ok(sudoku_vec) => {
                             self.sudoku = sudoku_vec;
+                            self.clues = self.sudoku.clone();
                             self.constraints.clear();
                             self.rendered_constraints = Vec::new();
                             self.solver = Solver::with_config("plain").unwrap();
