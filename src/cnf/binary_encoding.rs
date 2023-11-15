8f5bd9d0b043a7824f966c98c3a3eb8fc7c3f375
Vili Sinervä <vili.m.sinerva@gmail.com>
2023-11-15 16:57:42 +0200

    Add new files missing from last commit

---
+++ src/cnf/binary_encoding.rs
@@ -0,0 +1,282 @@
+use crate::cadical_wrapper::CadicalCallbackWrapper;
+use cadical::Solver;
+
+#[allow(dead_code)]
+pub fn sudoku_to_cnf(clues: &[Vec<Option<i32>>]) -> Vec<Vec<i32>> {
+    // each vec inside represents one cnf "statement"
+    let mut clauses: Vec<Vec<i32>> = Vec::new();
+
+    // every number in each row is different
+    for row in 1..=9 {
+        for col in 1..=9 {
+            for col2 in (col + 1)..=9 {
+                clauses.append(&mut eq_variable_init(row, col, row, col2));
+                clauses.push(vec![
+                    -eq_cnf_identifier(row, col, row, col2, 0),
+                    -eq_cnf_identifier(row, col, row, col2, 1),
+                    -eq_cnf_identifier(row, col, row, col2, 2),
+                    -eq_cnf_identifier(row, col, row, col2, 3),
+                ]);
+            }
+        }
+    }
+
+    // every number in each col is different
+    for col in 1..=9 {
+        for row in 1..=9 {
+            for row2 in (row + 1)..=9 {
+                clauses.append(&mut eq_variable_init(row, col, row2, col));
+                clauses.push(vec![
+                    -eq_cnf_identifier(row, col, row2, col, 0),
+                    -eq_cnf_identifier(row, col, row2, col, 1),
+                    -eq_cnf_identifier(row, col, row2, col, 2),
+                    -eq_cnf_identifier(row, col, row2, col, 3),
+                ]);
+            }
+        }
+    }
+
+    // each sub-grid has all the numbers
+    for subgrid_row in 0..=2 {
+        for subgrid_col in 0..=2 {
+            for index1 in 0..9 {
+                for index2 in (index1 + 1)..9 {
+                    let row = 1 + subgrid_row * 3 + index1 % 3;
+                    let col = 1 + subgrid_col * 3 + index1 / 3;
+                    let row2 = 1 + subgrid_row * 3 + index2 % 3;
+                    let col2 = 1 + subgrid_col * 3 + index2 / 3;
+                    clauses.append(&mut eq_variable_init(row, col, row2, col2));
+                    clauses.push(vec![
+                        -eq_cnf_identifier(row, col, row2, col2, 0),
+                        -eq_cnf_identifier(row, col, row2, col2, 1),
+                        -eq_cnf_identifier(row, col, row2, col2, 2),
+                        -eq_cnf_identifier(row, col, row2, col2, 3),
+                    ]);
+                }
+            }
+        }
+    }
+    // no numbers > 9
+    for row in 1..=9 {
+        for col in 1..=9 {
+            for forbidden in 9..16 {
+                let mut cell_clause = Vec::with_capacity(4);
+                let mut mask = 1;
+                for index in 0..4 {
+                    // Here we invert the bits, since we do not want to allow the forbidden numbers
+                    if (forbidden & mask) != 0 {
+                        cell_clause.push(-cnf_identifier(row, col, index));
+                    } else {
+                        cell_clause.push(cnf_identifier(row, col, index));
+                    }
+                    mask *= 2;
+                }
+                clauses.push(cell_clause);
+            }
+        }
+    }
+
+    // respect all the clues
+    for (row, line) in clues.iter().enumerate() {
+        for (col, val) in line.iter().enumerate() {
+            if let Some(mut val) = val {
+                val -= 1;
+                let mut mask = 1;
+                for index in 0..4 {
+                    if (val & mask) != 0 {
+                        clauses.push(vec![cnf_identifier(row as i32 + 1, col as i32 + 1, index)]);
+                    } else {
+                        clauses.push(vec![-cnf_identifier(row as i32 + 1, col as i32 + 1, index)]);
+                    }
+                    mask *= 2;
+                }
+            }
+        }
+    }
+
+    clauses
+}
+
+/// Initialize variables that indicate 2 cells have same bits in some position
+#[allow(dead_code)]
+fn eq_variable_init(row: i32, col: i32, row2: i32, col2: i32) -> Vec<Vec<i32>> {
+    let mut clauses: Vec<Vec<i32>> = Vec::new();
+
+    for bit in 0..4 {
+        clauses.push(vec![
+            -eq_cnf_identifier(row, col, row2, col2, bit),
+            cnf_identifier(row, col, bit),
+            -cnf_identifier(row2, col2, bit),
+        ]);
+
+        clauses.push(vec![
+            -eq_cnf_identifier(row, col, row2, col2, bit),
+            -cnf_identifier(row, col, bit),
+            cnf_identifier(row2, col2, bit),
+        ]);
+
+        clauses.push(vec![
+            eq_cnf_identifier(row, col, row2, col2, bit),
+            -cnf_identifier(row, col, bit),
+            -cnf_identifier(row2, col2, bit),
+        ]);
+
+        clauses.push(vec![
+            eq_cnf_identifier(row, col, row2, col2, bit),
+            cnf_identifier(row, col, bit),
+            cnf_identifier(row2, col2, bit),
+        ]);
+    }
+
+    clauses
+}
+
+#[allow(dead_code)] // allowed since binary encoding isn't used yet
+pub fn get_cell_value(solver: &Solver<CadicalCallbackWrapper>, row: i32, col: i32) -> i32 {
+    let mut value: i32 = 1;
+    for bit in 0..4 {
+        if solver.value(cnf_identifier(row, col, bit)).unwrap() {
+            value += 2_i32.pow(bit as u32);
+        }
+    }
+    value
+}
+
+#[inline(always)]
+pub fn cnf_identifier(row: i32, col: i32, bit: i32) -> i32 {
+    // Creates cnf identifier from sudoku based on row, column and value
+    // So every row, column and value combination has a unique identifier
+    (row - 1) * 4 * 9 + (col - 1) * 4 + bit + 1
+}
+
+#[inline(always)]
+pub fn eq_cnf_identifier(row: i32, col: i32, row2: i32, col2: i32, bit: i32) -> i32 {
+    9 * 9 * 4
+        + (row - 1) * 4 * 9 * 9 * 9
+        + (col - 1) * 4 * 9 * 9
+        + (row2 - 1) * 4 * 9
+        + (col2 - 1) * 4
+        + bit
+        + 1
+}
+
+/// These do not work for the new encoding YET, which is why they are not used YET
+#[allow(dead_code)]
+#[inline(always)]
+pub fn identifier_to_tuple(mut identifier: i32) -> (i32, i32, i32, bool) {
+    // Reverse CNF-identifier creation
+    // Return tuple of (row, col, bit_index, bit_value) from identifier
+    // bit_value will be false for negative ids, positive otherwise
+    let bit_value = identifier > 0;
+    identifier = identifier.abs() - 1;
+    (
+        identifier / (9 * 4) + 1,
+        (identifier % (9 * 4)) / 4 + 1,
+        (identifier % 4),
+        bit_value,
+    )
+}
+
+#[allow(dead_code)]
+pub fn eq_identifier_to_tuple(mut identifier: i32) -> (i32, i32, i32, i32, i32, bool) {
+    // Reverse CNF-identifier creation for equality constraints
+    // Return tuple of (row, col, row2, col2, bit_index, equal) from identifier
+    // equal will be false, if the bits in the two cells are different
+    let equal = identifier > 0;
+    identifier = identifier.abs() - 1 - 9 * 9 * 4;
+    (
+        identifier / (9 * 9 * 9 * 4) + 1,
+        (identifier % (9 * 9 * 9 * 4)) / (4 * 9 * 9) + 1,
+        (identifier % (9 * 9 * 4)) / (4 * 9) + 1,
+        (identifier % (9 * 4)) / 4 + 1,
+        (identifier % 4),
+        equal,
+    )
+}
+
+#[allow(dead_code)]
+pub fn create_tuples_from_constraints(
+    constraints: Vec<Vec<i32>>,
+) -> Vec<Vec<(i32, i32, i32, bool)>> {
+    let mut tuples = Vec::new();
+    for constraint in constraints.iter() {
+        let mut temp = Vec::with_capacity(constraint.len());
+        for value in constraint {
+            temp.push(identifier_to_tuple(*value));
+        }
+        tuples.push(temp);
+    }
+    tuples
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::*;
+
+    #[test]
+    fn no_overlapping_identifiers() {
+        let mut identifiers: Vec<i32> = Vec::new();
+        let mut identifiers_set: HashSet<i32> = HashSet::new();
+
+        for row in 1..=9 {
+            for col in 1..=9 {
+                for row2 in 1..=9 {
+                    for col2 in 1..=9 {
+                        for bit in 0..4 {
+                            identifiers_set.insert(eq_cnf_identifier(row, col, row2, col2, bit));
+                            identifiers.push(eq_cnf_identifier(row, col, row2, col2, bit));
+                        }
+                    }
+                }
+                for bit in 0..4 {
+                    identifiers_set.insert(cnf_identifier(row, col, bit));
+                    identifiers.push(cnf_identifier(row, col, bit));
+                }
+            }
+        }
+
+        assert_eq!(identifiers.len(), identifiers_set.len());
+    }
+
+    #[test]
+    fn test_to_id_and_back() {
+        assert_eq!(
+            (1, 1, 0, true),
+            identifier_to_tuple(cnf_identifier(1, 1, 0))
+        );
+        assert_eq!(
+            (1, 2, 3, true),
+            identifier_to_tuple(cnf_identifier(1, 2, 3))
+        );
+        assert_eq!(
+            (9, 9, 3, true),
+            identifier_to_tuple(cnf_identifier(9, 9, 3))
+        );
+        assert_eq!(
+            (6, 2, 0, false),
+            identifier_to_tuple(-1 * cnf_identifier(6, 2, 0))
+        );
+    }
+
+    #[test]
+    fn test_to_eq_id_and_back() {
+        assert_eq!(
+            (1, 1, 1, 1, 0, true),
+            eq_identifier_to_tuple(eq_cnf_identifier(1, 1, 1, 1, 0))
+        );
+        assert_eq!(
+            (1, 2, 3, 4, 0, true),
+            eq_identifier_to_tuple(eq_cnf_identifier(1, 2, 3, 4, 0))
+        );
+        assert_eq!(
+            (9, 9, 9, 9, 3, true),
+            eq_identifier_to_tuple(eq_cnf_identifier(9, 9, 9, 9, 3))
+        );
+        assert_eq!(
+            (6, 2, 8, 2, 0, false),
+            eq_identifier_to_tuple(-1 * eq_cnf_identifier(6, 2, 8, 2, 0))
+        );
+    }
+}
---
+++ src/sudoku.rs
@@ -0,0 +1,480 @@
+use crate::{app_state::EncodingType, CadicalCallbackWrapper, GenericError, Solver};
+use std::{fs, path::Path};
+
+pub fn solve_sudoku(
+    sudoku_clues: &[Vec<Option<i32>>],
+    solver: &mut Solver<CadicalCallbackWrapper>,
+    encoding: &EncodingType,
+) -> Result<Vec<Vec<Option<i32>>>, String> {
+    let mut solved: Vec<Vec<Option<i32>>> = Vec::new();
+    let cnf_clauses = encoding.sudoku_to_cnf(sudoku_clues);
+
+    for clause in cnf_clauses {
+        solver.add_clause(clause);
+    }
+
+    if let Some(true) = solver.solve() {
+        for row in 1..=9 {
+            let mut row_values = Vec::with_capacity(9);
+            for col in 1..=9 {
+                let value = encoding.get_cell_value(solver, row, col);
+                row_values.push(Some(value));
+            }
+            solved.push(row_values);
+        }
+        return Ok(solved);
+    }
+    Err(String::from("Solving sudoku failed!"))
+}
+
+pub fn get_sudoku(filename: String) -> Result<Vec<Vec<Option<i32>>>, GenericError> {
+    let sudoku_result = fs::read_to_string(filename);
+    match sudoku_result {
+        Ok(sudoku) => clues_from_string(sudoku, "."),
+        Err(_) => Err(GenericError {
+            msg: "Invalid filetype!".to_string(),
+        }),
+    }
+}
+
+pub fn write_sudoku(sudoku: String, path: &Path) -> Result<(), GenericError> {
+    let save_result = fs::write(path.display().to_string(), sudoku);
+    match save_result {
+        Err(_) => Err(GenericError {
+            msg: "Saving the file failed".to_string(),
+        }),
+        _ => Ok(()),
+    }
+}
+
+pub fn clues_from_string(
+    buf: String,
+    empty_value: &str,
+) -> Result<Vec<Vec<Option<i32>>>, GenericError> {
+    // Creates 2d Vec from string to represent clues found in sudoku
+    let mut clues: Vec<Vec<Option<i32>>> = Vec::with_capacity(9);
+    if buf.len() < 9 {
+        return Err(GenericError {
+            msg: "Invalid sudoku format!".to_owned(),
+        });
+    }
+    for line in buf.lines() {
+        let mut row_buf = Vec::with_capacity(9);
+        for val in line.split("") {
+            if val == empty_value {
+                row_buf.push(None)
+            }
+            if let Ok(val) = val.parse() {
+                row_buf.push(Some(val));
+            }
+        }
+        if row_buf.len() != 9 {
+            return Err(GenericError {
+                msg: "Invalid sudoku format!".to_owned(),
+            });
+        }
+        clues.push(row_buf);
+    }
+
+    Ok(clues)
+}
+
+pub fn string_from_grid(grid: Vec<Vec<Option<i32>>>) -> String {
+    let mut return_string = String::new();
+    for row in grid.iter().take(9) {
+        for col in row.iter().take(9) {
+            match col {
+                Some(v) => {
+                    return_string.push_str(&v.to_string());
+                }
+                None => {
+                    return_string.push('.');
+                }
+            }
+        }
+        return_string.push('\n');
+    }
+    return_string
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{ConstraintList, Trail};
+
+    #[test]
+    fn test_get_sudoku() {
+        let sudoku = get_sudoku("data/sample_sudoku.txt".to_string()).unwrap();
+        let should_be = vec![
+            vec![None, None, None, None, None, None, None, Some(1), None],
+            vec![Some(4), None, None, None, None, None, None, None, None],
+            vec![None, Some(2), None, None, None, None, None, None, None],
+            vec![
+                None,
+                None,
+                None,
+                None,
+                Some(5),
+                None,
+                Some(4),
+                None,
+                Some(7),
+            ],
+            vec![None, None, Some(8), None, None, None, Some(3), None, None],
+            vec![None, None, Some(1), None, Some(9), None, None, None, None],
+            vec![
+                Some(3),
+                None,
+                None,
+                Some(4),
+                None,
+                None,
+                Some(2),
+                None,
+                None,
+            ],
+            vec![None, Some(5), None, Some(1), None, None, None, None, None],
+            vec![None, None, None, Some(8), None, Some(6), None, None, None],
+        ];
+        assert_eq!(sudoku, should_be);
+    }
+
+    #[test]
+    fn test_solve_sudoku_decimal() {
+        let sudoku = get_sudoku("data/sample_sudoku.txt".to_string()).unwrap();
+        let mut solver = cadical::Solver::with_config("plain").unwrap();
+        let callback_wrapper = CadicalCallbackWrapper::new(ConstraintList::new(), Trail::new());
+        solver.set_callbacks(Some(callback_wrapper.clone()));
+
+        let solved = solve_sudoku(&sudoku, &mut solver, &EncodingType::Decimal).unwrap();
+        let should_be = vec![
+            vec![
+                Some(6),
+                Some(9),
+                Some(3),
+                Some(7),
+                Some(8),
+                Some(4),
+                Some(5),
+                Some(1),
+                Some(2),
+            ],
+            vec![
+                Some(4),
+                Some(8),
+                Some(7),
+                Some(5),
+                Some(1),
+                Some(2),
+                Some(9),
+                Some(3),
+                Some(6),
+            ],
+            vec![
+                Some(1),
+                Some(2),
+                Some(5),
+                Some(9),
+                Some(6),
+                Some(3),
+                Some(8),
+                Some(7),
+                Some(4),
+            ],
+            vec![
+                Some(9),
+                Some(3),
+                Some(2),
+                Some(6),
+                Some(5),
+                Some(1),
+                Some(4),
+                Some(8),
+                Some(7),
+            ],
+            vec![
+                Some(5),
+                Some(6),
+                Some(8),
+                Some(2),
+                Some(4),
+                Some(7),
+                Some(3),
+                Some(9),
+                Some(1),
+            ],
+            vec![
+                Some(7),
+                Some(4),
+                Some(1),
+                Some(3),
+                Some(9),
+                Some(8),
+                Some(6),
+                Some(2),
+                Some(5),
+            ],
+            vec![
+                Some(3),
+                Some(1),
+                Some(9),
+                Some(4),
+                Some(7),
+                Some(5),
+                Some(2),
+                Some(6),
+                Some(8),
+            ],
+            vec![
+                Some(8),
+                Some(5),
+                Some(6),
+                Some(1),
+                Some(2),
+                Some(9),
+                Some(7),
+                Some(4),
+                Some(3),
+            ],
+            vec![
+                Some(2),
+                Some(7),
+                Some(4),
+                Some(8),
+                Some(3),
+                Some(6),
+                Some(1),
+                Some(5),
+                Some(9),
+            ],
+        ];
+        assert_eq!(solved, should_be);
+    }
+
+    #[test]
+    fn test_solve_sudoku_binary() {
+        let sudoku = get_sudoku("data/sample_sudoku.txt".to_string()).unwrap();
+        let mut solver = cadical::Solver::with_config("plain").unwrap();
+        let callback_wrapper = CadicalCallbackWrapper::new(ConstraintList::new(), Trail::new());
+        solver.set_callbacks(Some(callback_wrapper.clone()));
+
+        let solved = solve_sudoku(&sudoku, &mut solver, &EncodingType::Binary).unwrap();
+        let should_be = vec![
+            vec![
+                Some(6),
+                Some(9),
+                Some(3),
+                Some(7),
+                Some(8),
+                Some(4),
+                Some(5),
+                Some(1),
+                Some(2),
+            ],
+            vec![
+                Some(4),
+                Some(8),
+                Some(7),
+                Some(5),
+                Some(1),
+                Some(2),
+                Some(9),
+                Some(3),
+                Some(6),
+            ],
+            vec![
+                Some(1),
+                Some(2),
+                Some(5),
+                Some(9),
+                Some(6),
+                Some(3),
+                Some(8),
+                Some(7),
+                Some(4),
+            ],
+            vec![
+                Some(9),
+                Some(3),
+                Some(2),
+                Some(6),
+                Some(5),
+                Some(1),
+                Some(4),
+                Some(8),
+                Some(7),
+            ],
+            vec![
+                Some(5),
+                Some(6),
+                Some(8),
+                Some(2),
+                Some(4),
+                Some(7),
+                Some(3),
+                Some(9),
+                Some(1),
+            ],
+            vec![
+                Some(7),
+                Some(4),
+                Some(1),
+                Some(3),
+                Some(9),
+                Some(8),
+                Some(6),
+                Some(2),
+                Some(5),
+            ],
+            vec![
+                Some(3),
+                Some(1),
+                Some(9),
+                Some(4),
+                Some(7),
+                Some(5),
+                Some(2),
+                Some(6),
+                Some(8),
+            ],
+            vec![
+                Some(8),
+                Some(5),
+                Some(6),
+                Some(1),
+                Some(2),
+                Some(9),
+                Some(7),
+                Some(4),
+                Some(3),
+            ],
+            vec![
+                Some(2),
+                Some(7),
+                Some(4),
+                Some(8),
+                Some(3),
+                Some(6),
+                Some(1),
+                Some(5),
+                Some(9),
+            ],
+        ];
+        assert_eq!(solved, should_be);
+    }
+
+    #[test]
+    fn test_get_no_sudoku() {
+        let test_file: String = "./data/foo_sudoku.txt".to_string();
+        let test_file_exists: bool = Path::new("./data/foo_sudoku.txt").exists();
+        let test_getting_sudoku = get_sudoku(test_file);
+
+        assert_eq!(test_file_exists, false);
+        assert!(test_getting_sudoku.is_err());
+    }
+
+    #[test]
+    fn test_get_wrong_filetype() {
+        let test_file: String = "./data/foo.exe".to_string();
+        let file_exists: bool = Path::new("./data/foo.exe").exists();
+        let assumed_error_message = "Invalid filetype!".to_string();
+        let test_result = get_sudoku(test_file);
+
+        assert_eq!(file_exists, false);
+        assert_eq!(test_result.err().unwrap().msg, assumed_error_message);
+    }
+
+    #[test]
+    fn test_write_sudoku() {
+        let test_text: String = "00000000".to_string();
+        let test_path: &Path = Path::new("./data/test_sudoku.txt");
+        let written = write_sudoku(test_text, &test_path);
+        let read_to_text = fs::read_to_string(test_path).unwrap();
+
+        assert!(written.is_ok());
+        assert_eq!(read_to_text, "00000000".to_string());
+    }
+
+    #[test]
+    fn test_write_no_sudoku() {
+        let test_text: String = "".to_string();
+        let test_path: &Path = Path::new("");
+        let written = write_sudoku(test_text, &test_path);
+
+        assert!(written.is_err());
+    }
+
+    #[test]
+    fn test_write_no_valid_path() {
+        let test_text2: String = "".to_string();
+        let test_path2: &Path = Path::new("./foo/foo.txt");
+        let path_exists: bool = test_path2.exists();
+        let assumed_error_message = "Saving the file failed".to_string();
+        let test_result = write_sudoku(test_text2, &test_path2);
+
+        assert_eq!(path_exists, false);
+        assert_eq!(test_result.err().unwrap().msg, assumed_error_message);
+    }
+
+    #[test]
+    fn test_string_to_clues() {
+        let test_sudoku = "..3......\n\
+                 1........\n\
+                 .........\n\
+                 .........\n\
+                 ..8......\n\
+                 .........\n\
+                 ......2..\n\
+                 .........\n\
+                 .....6...\n";
+
+        let clues = clues_from_string(test_sudoku.to_owned(), ".").unwrap();
+        assert_eq!(clues[0][2], Some(3));
+        assert_eq!(clues[1][0], Some(1));
+        assert_eq!(clues[4][2], Some(8));
+    }
+
+    #[test]
+    fn test_string_from_grid() {
+        let mut test_vec = vec![vec![<Option<i32>>::None; 9]; 9];
+        test_vec[0][0] = Some(1);
+        test_vec[1][1] = Some(2);
+        test_vec[2][2] = Some(3);
+        test_vec[3][3] = Some(4);
+        let output_string = string_from_grid(test_vec);
+
+        let test_string = "1........\n\
+                 .2.......\n\
+                 ..3......\n\
+                 ...4.....\n\
+                 .........\n\
+                 .........\n\
+                 .........\n\
+                 .........\n\
+                 .........\n";
+
+        assert_eq!(output_string, test_string)
+    }
+
+    #[test]
+    fn test_string_from_grid_second() {
+        let mut test_vec = vec![vec![<Option<i32>>::None; 9]; 9];
+        test_vec[0][5] = Some(1);
+        test_vec[2][3] = Some(2);
+        test_vec[4][7] = Some(3);
+        test_vec[7][5] = Some(4);
+        let output_string = string_from_grid(test_vec);
+
+        let test_string = ".....1...\n\
+                 .........\n\
+                 ...2.....\n\
+                 .........\n\
+                 .......3.\n\
+                 .........\n\
+                 .........\n\
+                 .....4...\n\
+                 .........\n";
+
+        assert_eq!(output_string, test_string)
+    }
+}
